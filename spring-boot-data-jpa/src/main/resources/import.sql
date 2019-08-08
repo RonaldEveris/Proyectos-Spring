b282831d18fcb156a5215a2e9d7ac8f016bd6643
RONALDBARON <baronsanchezronald@gmail.com>
2019-08-08 11:04:59 -0500

    first commit

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (1,'Andres','Guzman','profesor@bolsadeisdeas.com','2017-08-28');
+INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (2,'John','Doe','john.doe@gmail.com','2017-08-28');
+INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (3,'Brayan','Rondon','chamo2019@gmail.com','2019-12-23');
